f56cf764ee280276d65d1a0e20cd5d2880f96e55
zack <zckry.wlsn@gmail.com>
2022-03-08 10:49:43 +0000

    added function for who

--- math.py
+++ math.py
@@ -1,12 +1,13 @@
 
 def whois():
   while True:
-  answer = int(input("Hello who are you? "))
-  if answer == "Olive" || "olive" :
-      print ("Hello Olive. Get ready for some math! ")
-      break
-  elif answer == "Wilder" || "wilder" :
-      print ("Hellow Wilder. Get ready for some math! ")
-      break
-  else:
-      print ("This program knows about Olive and Wilder please spell your name correctly!")
+      answer = input("Hello who are you? ")
+      if answer == "Olive" or answer == "olive" :
+          print ("Hello Olive. Get ready for some math! ")
+          break
+      elif answer == "Wilder" or answer == "wilder" :
+          print ("Hellow Wilder. Get ready for some math! ")
+          break
+      else:
+          print ("This program knows about Olive and Wilder please spell your name correctly!")
+whois()
