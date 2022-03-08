8608f8f031815641979f7fb85faa24465b9f2b6b
zckrywlsn <zckry.wlsn@gmail.com>
2022-03-08 10:33:38 +0000

    Create math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+
+def whois():
+  while True
+  answer = int(input("Hello who are you? "))
+  if answer == "Olive" || "olive" :
+      print ("Hello Olive. Get ready for some math! ")
+      break
+  elif answer == "Wilder" || "wilder" :
+      print ("Hellow Wilder. Get ready for some math! ")
+      break
+  else:
+      print ("This program knows about Olive and Wilder please spell your name correctly!")
